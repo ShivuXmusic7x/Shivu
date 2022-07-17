4656b0cbcd83d95430725901e26bdd2db747a471
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-17 17:05:52 +0530

    Update speedtest.py

--- AnonX/plugins/Robot/speedtest.py
+++ AnonX/plugins/Robot/speedtest.py
@@ -2,8 +2,8 @@ import asyncio
 import speedtest
 from pyrogram import filters
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from AnonX import app
+from AnonX.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
