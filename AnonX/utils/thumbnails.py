99fc29a8523a44b8bbcece1eb53f17ae180d4a22
Mrvk1701 <91109120+Mrvk1701@users.noreply.github.com>
2022-09-18 16:10:40 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -2,8 +2,11 @@ import os
 import aiohttp
 import aiofiles
 from LUCKY.thumbnail import generate_thumb
-
+from LUCKY.thumbnail2 import generate_thumb
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 async def gen_thumb(videoid):
+    return await generate_thumb(videoid,MUSIC_BOT_NAME)
+
+async def gen_thumb2(videoid):
     return await generate_thumb(videoid,MUSIC_BOT_NAME)
